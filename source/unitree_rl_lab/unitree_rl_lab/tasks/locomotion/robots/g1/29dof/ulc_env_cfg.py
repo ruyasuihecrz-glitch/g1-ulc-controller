9fc3fb358493698ee70d5935cb37f8691af345c4
ruyasuihecrz <ruyasuihecrz@gmail.com>
2026-06-12 06:15:13 +0000

    Use mimic G1 hardware config for ULC training

--- source/unitree_rl_lab/unitree_rl_lab/tasks/locomotion/robots/g1/29dof/ulc_env_cfg.py
+++ source/unitree_rl_lab/unitree_rl_lab/tasks/locomotion/robots/g1/29dof/ulc_env_cfg.py
@@ -10,6 +10,7 @@ from isaaclab.utils import configclass
 from isaaclab.utils.noise import AdditiveUniformNoiseCfg as Unoise
 
 from unitree_rl_lab.tasks.locomotion import mdp
+from unitree_rl_lab.assets.robots.unitree import UNITREE_G1_29DOF_MIMIC_CFG
 from . import velocity_env_cfg as official_velocity
 
 
@@ -222,6 +223,9 @@ class ULCRobotEnvCfg(official_velocity.RobotEnvCfg):
 
     def __post_init__(self):
         super().__post_init__()
+        self.scene.robot = UNITREE_G1_29DOF_MIMIC_CFG.replace(
+            prim_path="{ENV_REGEX_NS}/Robot"
+        )
         self.scene.num_envs = 8192
         self.episode_length_s = 20.0
 
